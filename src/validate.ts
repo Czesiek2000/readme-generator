6da2b1a1903bc304ef852638c12931eb0122af01
Czesiek2000 <michal.czech00@gmail.com>
2021-03-21 19:27:43 +0100

    Add author, description validation

--- src/validate.ts
+++ src/validate.ts
@@ -7,3 +7,12 @@ export function isValid(input : string) : boolean | string {
     }
     return true
 }
+
+
+export function validateAuthor() : string{
+    return pkg.author.length === 0 ? os.userInfo().username : pkg.author
+}
+
+export function defaultDescription () {
+    return `This is default README description of ${pkg.name} project. This text should be replaced with your brief project description text.`
+}
