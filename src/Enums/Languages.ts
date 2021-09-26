e8e912bffe3d1954f73b2e5abac1d2cab90f2978
Czesiek2000 <michal.czech00@gmail.com>
2021-09-26 23:57:34 +0200

    Add more languages and emojis

--- src/Enums/Languages.ts
+++ src/Enums/Languages.ts
@@ -1,10 +1,12 @@
 export enum Languages {
     GIT = 'git',
+    NODE = 'node',
     VUE = 'vue',
     REACT = 'react',
     NEXT = 'next',
     NUXT = 'nuxt',
     GATSBY = 'gatsby',
+    TYPESCRIPT = 'typescript',
     JAVA = 'java',
     CPP = 'cpp',
 }
