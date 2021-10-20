f4348329b80d34faad92286d9bb838ed0f3e50fa
Czesiek2000 <michal.czech00@gmail.com>
2021-10-20 17:05:05 +0200

    Add interface for table of content

---
+++ src/Interfaces/Table.ts
@@ -0,0 +1,11 @@
+export interface Table {
+    included: boolean,
+    technologies: number,
+    prerequisite: boolean,
+    // instalation: boolean,
+    author: boolean,
+    contributors: number,
+    contributing: string,
+    previews?: number,
+    licence: boolean
+}
